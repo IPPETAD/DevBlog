c26ef7ce00b84f3e6624953e42ac5d6a73ad9087
James Finlay <jtfinlay@ualberta.ca>
2014-04-03 23:03:58 -0600

    MVC5 Project Initial Push

---
+++ DevBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevBlog.Startup))]
+namespace DevBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
